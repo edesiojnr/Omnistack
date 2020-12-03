cfa2a53bf91b0c34b2c37e52401e6b1b3644b772
edesiojnr <edesiojnr@gmail.com>
2020-12-03 18:57:36 -0300

    Finalizado

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,35 +1,57 @@
 import React from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { NumericLiteral } from 'typescript';
+import api from '../../services/api';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem(){
+export interface Teacher {
+       id: number;
+        avatar: string;
+        bio: string;
+        cost: number;   
+        name: string;
+        subject: string;
+        whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
     return (
     <article className="teacher-item">
                 <header>
-                    <img src="https://avatars1.githubusercontent.com/u/62727867?s=460&u=9c88ac92d1b2607ad833c235c3a6028984d96b95&v=4" alt="Edesio Junior"/>
+                    <img src={teacher.avatar} alt={teacher.name}/>
                     <div>
-                        <strong>Edesio Junior</strong>
-                        <span>Inglês</span>
+                        <strong>{teacher.name}</strong>
+                        <span>{teacher.subject}</span>
                     </div>
                 </header>
 
-                <p>
-                    Apaixonado por idiomas 
-                    <br /><br />
-                    Pretende conhecer vários países em seu ano sabatico
-                </p>
+                <p>{teacher.bio}</p>
 
                 <footer>
                     <p>
                         Preço/hora 
-                        <strong>R$ 100,00</strong>
+                        <strong>R$ {teacher.cost}</strong>
                     </p>
-                    <button type="button">
-                        <img src={whatsappIcon} alt="Whatsapp" />
-                        Entrar em contato
-                    </button>
+                    <a
+                     target="blank" onClick={createNewConnection} 
+                     href={`https://wa.me/${teacher.whatsapp}`}
+                     >
+                     <img src={whatsappIcon} alt="Whatsapp" />
+                     Entrar em contato
+                    </a>
                 </footer>
             </article>
  );
